18e723c591f939275434d44c4a1ffe5d1d57d948
Hari Babu <161557127+harikrishna87@users.noreply.github.com>
2024-09-19 21:46:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+let family_details = [
+    father = {
+        name: "Venkata Subbaiah",
+        age: 52,
+        occupation: "Farmer",
+        childrens: {
+            child1: {
+                name: "Hari Babu",
+                age: 23,
+                profession: "Student"
+            },
+            child2: {
+                name: "Vignesh",
+                age: 21,
+                profession: "Student"
+            }
+        }
+    },
+
+    mother = {
+        name: "Anuradha",
+        age: 42,
+        occupation: "HouseWife",
+        childrens: {
+            child1: {
+                name: "Hari Babu",
+                age: 23,
+                profession: "Student"
+            },
+            child2: {
+                name: "Vignesh",
+                age: 21,
+                profession: "Student"
+            }
+        }
+    },
+
+    Children1 = {
+        Father: "Venkata Subbaiah",
+        Mother: "Anuradha",
+        Name: "Hari Babu",
+        Age: 23,
+        profession: "Student",
+    },
+
+    Children2 = {
+        Father: "Venkata Subbaiah",
+        Mother: "Anuradha",
+        Name: "Vignesh",
+        Age: 21,
+        profession: "Student",
+    }
+]
+
+console.log(family_details[1]);
